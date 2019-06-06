44908ad21376939375c194c371144b35ff0d5276
Monil25 <monils1999@gmail.com>
2019-06-06 23:59:36 +0530

    allowing '*-' and '/-' to display

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-const btns = document.querySelectorAll('.btn');
-const screen = document.querySelector('.screen');
-const equalBtn = document.querySelector('.btn-equal');
-const clearBtn = document.querySelector('.btn-clear');
-const delbtn = document.querySelector('.btn-delete');
+const btns = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
+const equalBtn = document.querySelector(".btn-equal");
+const clearBtn = document.querySelector(".btn-clear");
+const delbtn = document.querySelector(".btn-delete");
 //const delbtn = document.querySelector('.btn-delete');
 const operands = ["*", "+", "/", "-"];
+const exceptThis = ["/-", "*-"];
 for (let i = 0; i < btns.length; i++) {
-	btns[i].addEventListener('click', function () {
-		let number = btns[i].getAttribute('data-num');
-		console.log(typeof number);
+	btns[i].addEventListener("click", function () {
+		let number = btns[i].getAttribute("data-num");
 		let outputStr = screen.value;
 		let len = outputStr.length;
 		let lastChar = outputStr[len - 1];
-		if (len > 0) { //EXAMPLE:-IF "*" CLICKED JUST AFTER "+", REMOVE "+" AND REPLACE WITH "*"
-			if (operands.includes(lastChar) && operands.includes(number)) {
-				screen.value = (outputStr.substring(0, len - 1) + number);
+		if (len > 0) {
+			//EXAMPLE:-IF "*" CLICKED JUST AFTER "+", REMOVE "+" AND REPLACE WITH "*"
+			//but not for 8*-5= -40, 8/-2=-4.
+			let lastTwo = outputStr.charAt(len - 1) + number;
+			if (
+				operands.includes(lastChar) &&
+				operands.includes(number) &&
+				!exceptThis.includes(lastTwo)
+			) {
+				screen.value = outputStr.substring(0, len - 1) + number;
 			} else {
 				screen.value += number;
 			}
 		} else {
 			screen.value += number;
 		}
-	})
+	});
 }
 /*here "innerHTML" cannot be used because
 
@@ -49,26 +56,21 @@ innerHTML of the div tag would be the string:
 
 */
 
-
-equalBtn.addEventListener('click', function () {
-	if (screen.value === '')
-		alert("No expression was entered!");
+equalBtn.addEventListener("click", function () {
+	if (screen.value === "") alert("No expression was entered!");
 	else {
 		try {
 			let value = eval(screen.value);
 			screen.value = value;
 		} catch (e) {
-			if (e instanceof SyntaxError)
-				alert("You entered a wrong expression!");
+			if (e instanceof SyntaxError) alert("You entered a wrong expression!");
 		}
 	}
-
-})
-delbtn.addEventListener('click', function () {
+});
+delbtn.addEventListener("click", function () {
 	var len = screen.value.length;
-	if (len > 0)
-		screen.value = screen.value.substring(0, len - 1);
-})
-clearBtn.addEventListener('click', function () {
-	screen.value = '';
-})
+	if (len > 0) screen.value = screen.value.substring(0, len - 1);
+});
+clearBtn.addEventListener("click", function () {
+	screen.value = "";
+});
